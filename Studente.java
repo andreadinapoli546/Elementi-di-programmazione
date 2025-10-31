112d7b8ee86dd6b0931293cbc997e072e8727fed
andreadinapoli546 <andrea.di.napoli@edu.immaginazioneelavoro.it>
2025-10-31 15:42:52 +0100

    Update Studente.java

--- Studente.java
+++ Studente.java
@@ -1,5 +1,38 @@
 package Corso;
 
 public class Studente {
+	
+	    private String nome;
+	    private String cognome;
+	    private String codFiscale;
+	    private String corsoIscritto;
+
+	    public Studente(String nome, String cognome, String codFiscale, String corsoIscritto) {
+	        this.nome = nome;
+	        this.cognome = cognome;
+	        this.codFiscale = codFiscale;
+	        this.corsoIscritto = corsoIscritto;
+	    }
+
+	    public String getNome() { return nome; }
+	    public String getCognome() { return cognome; }
+	    public String getCodFiscale() { return codFiscale; }
+	    public String getCorsoIscritto() { return corsoIscritto; }
+
+	    
+	    public String toString() {
+	        return "Studente [Nome=" + nome + ", Cognome=" + cognome + ", CF=" + codFiscale + ", Corso=" + corsoIscritto + "]";
+	    }
+
+	    public String toCSV() {
+	        return nome + ";" + cognome + ";" + codFiscale + ";" + corsoIscritto;
+	    }
+
+	    public static Studente fromCSV(String line) {
+	        String[] parts = line.split(";");
+	        return new Studente(parts[0], parts[1], parts[2], parts[3]);
+	    }
+	}
+
 
 }
